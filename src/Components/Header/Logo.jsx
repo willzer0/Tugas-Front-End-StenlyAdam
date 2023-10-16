dcd75e8c5585e2927843815dc9364b89bbc812fc
willzer0 <pembunuh4444@gmail.com>
2023-10-16 10:36:37 +0700

    Revert "menambah logo"
    This reverts commit fb9727fb711d7250d64db2ffbd971cf60df863f6.

--- src/Components/Header/Logo.jsx
+++
@@ -1,4 +0,0 @@
-const Logo = () => {
-  return <h1>ini Logo</h1>;
-};
-export default Logo;
--- src/Components/Header/index.jsx
+++
@@ -1,6 +0,0 @@
-import Logo from "./Logo";
-const Header = () => {
-  return <Logo />;
-};
-
-export default Header;
