af0614cc9ab822b53adfe2956b0527ee221f6ad3
shelisart <shellsha.salainti@gmail.com>
2023-10-16 16:45:09 +0800

    menambahkan search pada home

---
+++ src/Components/Home/index.jsx
@@ -0,0 +1,13 @@
+import Search from './Search';
+import search from './Search';
+
+const index = () => {
+    return (
+    <>
+        <h1>Home</h1>
+        <Search />
+    </>
+    );
+};
+
+export default index;
