4a358ce6ee708776b24b8bd47c0d8cf3dcb112f3
AndreasTopuh <andreasjeno23@gmail.com>
2023-10-17 12:28:54 +0800

    menambahkan file card-info dari Andreas

---
+++ src/Components/Home/card-info.jsx
@@ -0,0 +1,5 @@
+const Card_info = () => {
+    return <h1>Card-info</h1>
+}
+
+export default Card_info;
--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -1,11 +1,13 @@
 import Search from './Search';
 import search from './Search';
+import Card_info from './card-info';
 
 const index = () => {
     return (
     <>
         <h1>Home</h1>
         <Search />
+        <Card_info />
     </>
     );
 };
