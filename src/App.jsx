2c4a7160477397986ebc6ce1681581ae00f19f66
gracecilia24 <s22110291@student.unklab.ac.id>
2023-10-18 09:26:03 +0800

    mengubah index

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Header from "./Components/Header";
 import Home from "./Components/Home";
-
+import Footer from "./Components/Footer"
 function App() {
   return (
     <>
       <Header />
       <Home />
+      <Footer />
     </>
   );
 }
---
+++ src/Components/Footer/index.jsx
@@ -0,0 +1,5 @@
+const footer = () => {
+    return <h1>ini footer di tulis oleh grace</h1>;
+}
+
+export default footer
