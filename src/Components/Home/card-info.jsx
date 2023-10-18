bcf204a1000bba833b0ea2bbaf3ba3b13b3fe09d
AndreasTopuh <andreasjeno23@gmail.com>
2023-10-18 09:47:55 +0800

    selesaikan tugas Card-info

--- src/Components/Home/card-info.jsx
+++ src/Components/Home/card-info.jsx
@@ -1,5 +1,21 @@
+import Evolution from "./evolution.jsx";
+import Logo from "./Logo.jsx";
+import Type_effect from "./Type_effect";
+
 const Card_info = () => {
-    return <h1>Card-info</h1>
+    return (
+
+    <>
+        <h1>Card-info</h1>
+ 
+        <Evolution />
+        <Logo />
+        <Type_effect />
+    </>
+  
+    )
+
+    
 }
 
 export default Card_info;
