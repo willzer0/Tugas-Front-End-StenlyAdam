078e60cb3b5ba4b8d34a2ba007f4facd4cf657ef
gracecilia24 <s22110291@student.unklab.ac.id>
2023-10-18 17:50:54 +0800

    fixx footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./Components/Header";
 import Home from "./Components/Home";
-import Footer from "./Components/Footer"
+import Footer from "./Components/Footer";
+
 function App() {
   return (
     <>
--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -2,4 +2,4 @@ const Footer = () => {
     return <h1>ini footer di tulis oleh grace</h1>;
 }
 
-export default footer
+export default Footer;
